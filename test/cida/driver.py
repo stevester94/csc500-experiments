c7885dfd02f1a14c33293a3a81677cccfc1cec9e
Steven Mackey <stevester94@gmail.com>
2021-09-29 23:02:16 -0700

    granular mnist and splitting ds (ITS LEARNING)

--- test/cida/driver.py
+++ test/cida/driver.py
@@ -8,6 +8,7 @@ import os
 import sys
 import json
 import time
+from math import floor
 
 # Parameters relevant to results
 RESULTS_DIR = "./results"
@@ -29,7 +30,7 @@ elif len(sys.argv) == 1:
     fake_args = {}
     fake_args["experiment_name"] = "Manual Experiment"
     fake_args["lr"] = 0.0001
-    fake_args["n_epoch"] = 10
+    fake_args["n_epoch"] = 50
     fake_args["batch_size"] = 128
     fake_args["patience"] = 10
     fake_args["seed"] = 1337
@@ -52,23 +53,68 @@ start_time_secs = time.time()
 # Build the dataset
 ###################################
 source_ds = CIDA_MNIST_DS(
-    seed, 
-    5, 
-    0,
-    100,
-    1000
+    seed,
+    [ 
+        {
+            "domain_index":0,
+            "min_rotation_degrees":0,
+            "max_rotation_degrees":10,
+            "num_examples_in_domain":10000,
+        },
+        {
+            "domain_index":1,
+            "min_rotation_degrees":11,
+            "max_rotation_degrees":20,
+            "num_examples_in_domain":10000,
+        },
+        {
+            "domain_index":2,
+            "min_rotation_degrees":21,
+            "max_rotation_degrees":30,
+            "num_examples_in_domain":10000,
+        },
+        {
+            "domain_index":3,
+            "min_rotation_degrees":31,
+            "max_rotation_degrees":40,
+            "num_examples_in_domain":10000,
+        },
+    ]
 )
 
 target_ds = CIDA_MNIST_DS(
     seed, 
-    5, 
-    100,
-    180,
-    1000
+    [
+        {
+            "domain_index":4,
+            "min_rotation_degrees":41,
+            "max_rotation_degrees":75,
+            "num_examples_in_domain":10000,
+        },
+        {
+            "domain_index":5,
+            "min_rotation_degrees":76,
+            "max_rotation_degrees":90,
+            "num_examples_in_domain":10000,
+        },
+        {
+            "domain_index":6,
+            "min_rotation_degrees":91,
+            "max_rotation_degrees":130,
+            "num_examples_in_domain":10000,
+        },
+        {
+            "domain_index":7,
+            "min_rotation_degrees": 131,
+            "max_rotation_degrees":180,
+            "num_examples_in_domain":10000,
+        },
+    ]
 )
 
-source_dl = torch.utils.data.DataLoader(
-    source_ds,
+def wrap_in_dataloader(ds):
+    return torch.utils.data.DataLoader(
+    ds,
     batch_size=batch_size,
     shuffle=True,
     num_workers=1,
@@ -77,14 +123,21 @@ source_dl = torch.utils.data.DataLoader(
     pin_memory=True
 )
 
-target_dl = torch.utils.data.DataLoader(
-    target_ds,
-    batch_size=batch_size,
-    shuffle=True,
-    num_workers=1,
-    persistent_workers=True,
-    prefetch_factor=50,
-    pin_memory=True
+
+source_train_len = floor(len(source_ds)*0.7)
+source_val_len   = floor(len(source_ds)*0.15)
+source_test_len  = len(source_ds) - source_train_len - source_val_len
+source_train, source_val, source_test = torch.utils.data.random_split(source_ds, [source_train_len, source_val_len, source_test_len], generator=torch.Generator().manual_seed(seed))
+source_train, source_val, source_test = (
+    wrap_in_dataloader(source_train), wrap_in_dataloader(source_val), wrap_in_dataloader(source_test)
+)
+
+target_train_len = floor(len(target_ds)*0.7)
+target_val_len   = floor(len(target_ds)*0.15)
+target_test_len  = len(target_ds) - target_train_len - target_val_len
+target_train, target_val, target_test = torch.utils.data.random_split(target_ds, [target_train_len, target_val_len, target_test_len], generator=torch.Generator().manual_seed(seed))
+target_train, target_val, target_test = (
+    wrap_in_dataloader(target_train), wrap_in_dataloader(target_val), wrap_in_dataloader(target_test)
 )
 
 def sigmoid(epoch, total_epochs):
@@ -124,10 +177,10 @@ cida_tet_jig = CIDA_Train_Eval_Test_Jig(
 )
 
 cida_tet_jig.train(
-    source_train_iterable=source_dl,
-    source_val_iterable=source_dl,
-    target_train_iterable=target_dl,
-    target_val_iterable=target_dl,
+    source_train_iterable=source_train,
+    source_val_iterable=source_val,
+    target_train_iterable=target_train,
+    target_val_iterable=target_val,
     patience=patience,
     learning_rate=lr,
     num_epochs=n_epoch,
@@ -139,8 +192,8 @@ cida_tet_jig.train(
 ###################################
 # Colate experiment results
 ###################################
-source_test_label_accuracy, source_test_label_loss, source_test_domain_loss = cida_tet_jig.test(source_dl)
-target_test_label_accuracy, target_test_label_loss, target_test_domain_loss = cida_tet_jig.test(target_dl)
+source_test_label_accuracy, source_test_label_loss, source_test_domain_loss = cida_tet_jig.test(source_test)
+target_test_label_accuracy, target_test_label_loss, target_test_domain_loss = cida_tet_jig.test(target_test)
 
 history = cida_tet_jig.get_history()
 
