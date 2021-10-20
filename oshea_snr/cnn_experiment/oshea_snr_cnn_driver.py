7720139858f033e500df04e20847ca83351a944c
Steven Mackey <stevester94@gmail.com>
2021-10-19 22:28:02 -0700

    Add snr breakdown to cnn driver

--- oshea_snr/cnn_experiment/oshea_snr_cnn_driver.py
+++ oshea_snr/cnn_experiment/oshea_snr_cnn_driver.py
@@ -14,7 +14,7 @@ import matplotlib.gridspec
 from steves_utils.oshea_RML2016_ds import OShea_RML2016_DS
 from steves_utils.lazy_map import Lazy_Map
 from math import floor
-
+import pandas as pds
 
 
 # Parameters relevant to results
@@ -38,7 +38,7 @@ elif len(sys.argv) == 1:
     fake_args = {}
     fake_args["experiment_name"] = "OShea SNR CNN"
     fake_args["lr"] = 0.001
-    fake_args["n_epoch"] = 200
+    fake_args["n_epoch"] = 100
     fake_args["batch_size"] = 128
     fake_args["patience"] = 10
     fake_args["seed"] = 1337
@@ -73,7 +73,7 @@ n_epoch                    = parameters["n_epoch"]
 batch_size                 = parameters["batch_size"]
 patience                   = parameters["patience"]
 seed                       = parameters["seed"]
-device                     = parameters["device"]
+device                     = torch.device(parameters["device"])
 source_snrs                = parameters["source_snrs"]
 target_snrs                = parameters["target_snrs"]
 normalize_domain           = parameters["normalize_domain"]
@@ -112,21 +112,19 @@ else:
 # This gives us a final tuple of
 # (Time domain IQ, label)
 source_ds = OShea_RML2016_DS(
-    normalize_snr=nrml,
     snrs_to_get=source_snrs,
 )
 
-source_ds = Lazy_Map(
-    source_ds, lambda i: i[:2]
-)
+# source_ds = Lazy_Map(
+#     source_ds, lambda i: i[:2]
+# )
 
 target_ds = OShea_RML2016_DS(
-    normalize_snr=nrml,
     snrs_to_get=target_snrs,
 )
-target_ds = Lazy_Map(
-    target_ds, lambda i: i[:2]
-)
+# target_ds = Lazy_Map(
+#     target_ds, lambda i: i[:2]
+# )
 
 
 
@@ -146,23 +144,37 @@ def wrap_in_dataloader(ds):
 source_train_len = floor(len(source_ds)*0.7)
 source_val_len   = floor(len(source_ds)*0.15)
 source_test_len  = len(source_ds) - source_train_len - source_val_len
-source_train, source_val, source_test = torch.utils.data.random_split(source_ds, [source_train_len, source_val_len, source_test_len], generator=torch.Generator().manual_seed(seed))
-source_val, source_test = (
-    wrap_in_dataloader(source_val), wrap_in_dataloader(source_test)
-)
+source_train_ds, source_val_ds, source_test_ds = torch.utils.data.random_split(source_ds, [source_train_len, source_val_len, source_test_len], generator=torch.Generator().manual_seed(seed))
+
 
 target_train_len = floor(len(target_ds)*0.7)
 target_val_len   = floor(len(target_ds)*0.15)
 target_test_len  = len(target_ds) - target_train_len - target_val_len
-target_train, target_val, target_test = torch.utils.data.random_split(target_ds, [target_train_len, target_val_len, target_test_len], generator=torch.Generator().manual_seed(seed))
-target_val, target_test = (
-    wrap_in_dataloader(target_val), wrap_in_dataloader(target_test)
+target_train_ds, target_val_ds, target_test_ds = torch.utils.data.random_split(target_ds, [target_train_len, target_val_len, target_test_len], generator=torch.Generator().manual_seed(seed))
+
+# Properly form the data using a transform lambda and Lazy_Map. Finally wrap them in a dataloader
+# Train ds gets treated a little special in cida, we mirror the structure here
+
+transform_lambda = lambda ex: ex[:2]
+
+train_ds = source_train_ds
+train_dl = wrap_in_dataloader(
+    Lazy_Map(train_ds, transform_lambda)
 )
 
-train = source_train
-train = wrap_in_dataloader(train)
+source_val_dl = wrap_in_dataloader(
+    Lazy_Map(source_val_ds, transform_lambda)
+)
+source_test_dl = wrap_in_dataloader(
+    Lazy_Map(source_test_ds, transform_lambda)
+)
 
-
+target_val_dl = wrap_in_dataloader(
+    Lazy_Map(target_val_ds, transform_lambda)
+)
+target_test_dl  = wrap_in_dataloader(
+    Lazy_Map(target_test_ds, transform_lambda)
+)
 
 
 ###################################
@@ -181,13 +193,13 @@ model = Configurable_Vanilla(
 vanilla_tet_jig = Vanilla_Train_Eval_Test_Jig(
     model=model,
     path_to_best_model=BEST_MODEL_PATH,
-    device=torch.device(device),
+    device=device,
     label_loss_object=torch.nn.NLLLoss(),
 )
 
 vanilla_tet_jig.train(
-    train_iterable=train,
-    val_iterable=source_val,
+    train_iterable=train_dl,
+    val_iterable=source_val_dl,
     patience=patience,
     num_epochs=n_epoch,
     num_logs_per_epoch=NUM_LOGS_PER_EPOCH,
@@ -197,16 +209,84 @@ vanilla_tet_jig.train(
 ###################################
 # Colate experiment results
 ###################################
-source_test_label_accuracy, source_test_label_loss = vanilla_tet_jig.test(source_test)
-target_test_label_accuracy, target_test_label_loss = vanilla_tet_jig.test(target_test)
+source_test_label_accuracy, source_test_label_loss = vanilla_tet_jig.test(source_test_dl)
+target_test_label_accuracy, target_test_label_loss = vanilla_tet_jig.test(target_test_dl)
 
-source_val_label_accuracy, source_val_label_loss = vanilla_tet_jig.test(source_val)
-target_val_label_accuracy, target_val_label_loss = vanilla_tet_jig.test(target_val)
+source_val_label_accuracy, source_val_label_loss = vanilla_tet_jig.test(source_val_dl)
+target_val_label_accuracy, target_val_label_loss = vanilla_tet_jig.test(target_val_dl)
 
 history = vanilla_tet_jig.get_history()
 
 total_epochs_trained = len(history["epoch_indices"])
+
+
+
+def predict(model, device, batch):
+
+    x,y,u = batch
+    x = x.to(device)
+    y = y.to(device)
+    u = u.to(device)
+    y_hat = model.forward(x)
+    pred = y_hat.data.max(1, keepdim=True)[1]
+    pred = torch.flatten(pred).cpu()
+
+    return pred
+
+def accuracy_by_domain(dict, dl):
+    for batch in dl:
+        pred = predict(model, device, batch)
+
+        X,Y,U = batch
+
+        for i in range(X.shape[0]):
+            x,y,u = (X[i],Y[i],U[i])
+            p = pred[i]
+
+            x = x.cpu().detach().numpy()
+            y = y.cpu().detach().numpy()
+            u = tuple(u.cpu().detach().numpy().tolist())
+            p = p.cpu().detach().numpy()
+
+            if y == p: dict[u][0] += 1
+            else: dict[u][1] += 1
+
+source_val_dl = wrap_in_dataloader(source_val_ds)
+target_val_dl = wrap_in_dataloader(target_val_ds)
+
+source_count_by_domain = {}
+target_count_by_domain = {}
+
+for s in source_snrs:
+    source_count_by_domain[(s,)] = [0,0]
+
+for s in target_snrs:
+    target_count_by_domain[(s,)] = [0,0]
+
+
+accuracy_by_domain(source_count_by_domain, source_val_dl)
+accuracy_by_domain(target_count_by_domain, target_val_dl)
+
+accuracies_by_domain = {
+    "source": [],
+    "accuracy": [],
+    "domain": [],
+}
+
+for key,val in source_count_by_domain.items():
+    accuracies_by_domain["source"].append(True)
+    accuracies_by_domain["accuracy"].append(val[0] / (val[0] + val[1]))
+    accuracies_by_domain["domain"].append(key[0]) # Breaking the assumption of vector domains here
+
+for key,val in target_count_by_domain.items():
+    accuracies_by_domain["source"].append(False)
+    accuracies_by_domain["accuracy"].append(val[0] / (val[0] + val[1]))
+    accuracies_by_domain["domain"].append(key[0]) # Breaking the assumption of vector domains here
+
+
 total_experiment_time_secs = time.time() - start_time_secs
+
+
 
 experiment = {
     "experiment_name": experiment_name,
@@ -222,6 +302,7 @@ experiment = {
         "target_val_label_loss": target_val_label_loss,
         "total_epochs_trained": total_epochs_trained,
         "total_experiment_time_secs": total_experiment_time_secs,
+        "val_accuracies_by_domain": accuracies_by_domain 
     },
     "history": history,
 }
@@ -295,6 +376,16 @@ t = ax.table(
 t.auto_set_font_size(False)
 t.set_fontsize(20)
 t.scale(1.5, 2)
+
+#
+# Build a damn pandas dataframe and plot it
+# 
+ax = fig.add_subplot(gs[0,2])
+df = pds.DataFrame.from_dict(accuracies_by_domain)
+df = df.sort_values("domain")
+df = df.pivot(index="domain", columns="source", values="accuracy")
+df.plot(kind="bar", ax=ax)
+
 
 if not (len(sys.argv) > 1 and sys.argv[1] == "-"):
     plt.show()
