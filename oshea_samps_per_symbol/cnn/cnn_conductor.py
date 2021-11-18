daba32f2a6d1748db7943b4fd9e6f1ba1a521bbd
Steven Mackey <stevester94@gmail.com>
2021-11-18 14:00:23 -0800

    OShea samps per symbol conductor looking good

---
+++ oshea_samps_per_symbol/cnn/cnn_conductor.py
@@ -0,0 +1,80 @@
+#! /usr/bin/env python3
+import json
+
+###########################################
+# Build all experiment json parameters
+###########################################
+experiment_jsons = []
+
+base_parameters = {}
+
+base_parameters["experiment_name"] = "SampsPerSymbol Train One Test Several Trial 1"
+base_parameters["lr"] = 0.0001
+base_parameters["n_epoch"] = 5
+base_parameters["batch_size"] = 256
+base_parameters["patience"] = 10
+base_parameters["device"] = "cuda"
+base_parameters["source_domains"] = [4]
+base_parameters["target_domains"] = [2,6,8,10]
+
+seeds = [
+    1337,
+    5748,
+    14195,
+    15493,
+    # 14209,
+    # 15572,
+    # 43,
+    # 179,
+    # 1316,
+    # 6948,
+    # 3854,
+    # 12698,
+    # 15124,
+    # 4954,
+    # 5578,
+    # 3764,
+]
+
+
+base_parameters["x_net"] = [
+    {"class": "Conv1d", "kargs": { "in_channels":2, "out_channels":50, "kernel_size":7, "stride":1, "padding":0},},
+    {"class": "ReLU", "kargs": {"inplace": True}},
+    {"class": "Conv1d", "kargs": { "in_channels":50, "out_channels":50, "kernel_size":7, "stride":1, "padding":0 },},
+    {"class": "ReLU", "kargs": {"inplace": True}},
+    {"class": "Dropout", "kargs": {"p": 0.5}},
+    {"class": "Flatten", "kargs": {}},
+
+    {"class": "Linear", "kargs": {"in_features": 5800, "out_features": 256}},
+    {"class": "ReLU", "kargs": {"inplace": True}},
+    {"class": "Dropout", "kargs": {"p": 0.5}},
+
+    {"class": "Linear", "kargs": {"in_features": 256, "out_features": 80}},
+    {"class": "ReLU", "kargs": {"inplace": True}},
+    {"class": "Dropout", "kargs": {"p": 0.5}},
+
+    {"class": "Linear", "kargs": {"in_features": 80, "out_features": 16}},
+]
+
+
+import copy
+for seed in seeds:
+
+    parameters = copy.deepcopy(base_parameters)
+
+    parameters["seed"] = seed
+
+    j = json.dumps(parameters, indent=2)
+    experiment_jsons.append(j)
+
+###########################################
+# Run all experiments using Conductor
+###########################################
+import os
+from steves_utils.conductor import Conductor
+
+conductor = Conductor(
+    TRIALS_BASE_PATH=os.path.realpath(os.path.join("./results/", "SampsPerSymbolTrainOneTestSeveral", "trial_1")),
+    EXPERIMENT_PATH="./experiment"
+)
+conductor.conduct_experiments(experiment_jsons)
