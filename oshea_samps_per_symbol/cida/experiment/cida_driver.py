adc6fab5064365bbc642d2cf68a83749aeaf89df
Steven Mackey <stevester94@gmail.com>
2021-11-19 21:56:01 -0800

    [sampersym] alpha 0 but it learns

--- oshea_samps_per_symbol/cida/experiment/cida_driver.py
+++ oshea_samps_per_symbol/cida/experiment/cida_driver.py
@@ -51,7 +51,7 @@ elif len(sys.argv) == 1:
     base_parameters["experiment_name"] = "Manual Experiment"
     base_parameters["lr"] = 0.001
     base_parameters["n_epoch"] = 20
-    base_parameters["batch_size"] = 256
+    base_parameters["batch_size"] = 128
     base_parameters["patience"] = 10
     base_parameters["seed"] = 1337
     base_parameters["device"] = "cuda"
@@ -59,6 +59,42 @@ elif len(sys.argv) == 1:
     base_parameters["source_domains"] = [8]
     base_parameters["target_domains"] = [8]
 
+    # Original, does not work
+    # base_parameters["x_net"] = [
+    #     {"class": "Conv1d", "kargs": { "in_channels":2, "out_channels":50, "kernel_size":7, "stride":1, "padding":0 },},
+    #     {"class": "ReLU", "kargs": {"inplace": True}},
+    #     {"class": "Conv1d", "kargs": { "in_channels":50, "out_channels":50, "kernel_size":7, "stride":2, "padding":0 },},
+    #     {"class": "ReLU", "kargs": {"inplace": True}},
+    #     {"class": "Dropout", "kargs": {"p": 0.5}},
+    #     {"class": "Flatten", "kargs": {}},
+    # ]
+    # base_parameters["u_net"] = [
+    #     {"class": "nnReshape", "kargs": {"shape":[-1, 1]}},
+    # ]
+    # base_parameters["merge_net"] = [
+    #     {"class": "Linear", "kargs": {"in_features": 50*58+1, "out_features": 256}},
+    #     {"class": "ReLU", "kargs": {"inplace": True}},
+    #     {"class": "Dropout", "kargs": {"p": 0.5}},
+    # ]
+    # base_parameters["class_net"] = [
+    #     # {"class": "Linear", "kargs": {"in_features": 256, "out_features": 256}},
+    #     # {"class": "ReLU", "kargs": {"inplace": True}},
+    #     # {"class": "Dropout", "kargs": {"p": 0.5}},
+
+    #     {"class": "Linear", "kargs": {"in_features": 256, "out_features": 80}},
+    #     {"class": "ReLU", "kargs": {"inplace": True}},
+
+    #     {"class": "Linear", "kargs": {"in_features": 80, "out_features": 16}},
+    # ]
+    # base_parameters["domain_net"] = [
+    #     {"class": "Linear", "kargs": {"in_features": 256, "out_features": 1}},
+
+    #     # {"class": "Linear", "kargs": {"in_features": 256, "out_features": 100}},
+    #     # {"class": "BatchNorm1d", "kargs": {"num_features": 100}},
+    #     # {"class": "ReLU", "kargs": {"inplace": True}},
+    #     # {"class": "Linear", "kargs": {"in_features": 100, "out_features": 1}},
+    #     # # {"class": "Flatten", "kargs": {"start_dim":0}},
+    # ]
 
     base_parameters["x_net"] = [
         {"class": "Conv1d", "kargs": { "in_channels":2, "out_channels":50, "kernel_size":7, "stride":1, "padding":0 },},
@@ -70,9 +106,11 @@ elif len(sys.argv) == 1:
     ]
     base_parameters["u_net"] = [
         {"class": "nnReshape", "kargs": {"shape":[-1, 1]}},
+        {"class": "Linear", "kargs": {"in_features": 1, "out_features": 10}},
+        # {"class": "nnMultiply", "kargs": {"constant":0}},
     ]
     base_parameters["merge_net"] = [
-        {"class": "Linear", "kargs": {"in_features": 50*58+1, "out_features": 256}},
+        {"class": "Linear", "kargs": {"in_features": 50*58+10, "out_features": 256}},
         {"class": "ReLU", "kargs": {"inplace": True}},
         {"class": "Dropout", "kargs": {"p": 0.5}},
     ]
@@ -84,7 +122,7 @@ elif len(sys.argv) == 1:
         {"class": "Linear", "kargs": {"in_features": 256, "out_features": 80}},
         {"class": "ReLU", "kargs": {"inplace": True}},
 
-        {"class": "Linear", "kargs": {"in_features": 80, "out_features": 16}},
+        {"class": "Linear", "kargs": {"in_features": 80, "out_features": 9}},
     ]
     base_parameters["domain_net"] = [
         {"class": "Linear", "kargs": {"in_features": 256, "out_features": 1}},
@@ -95,6 +133,8 @@ elif len(sys.argv) == 1:
         # {"class": "Linear", "kargs": {"in_features": 100, "out_features": 1}},
         # # {"class": "Flatten", "kargs": {"start_dim":0}},
     ]
+
+
 
     base_parameters["device"] = "cuda"
 
