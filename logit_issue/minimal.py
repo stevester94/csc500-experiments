ab3cc455f3aa32eb2899b8930c517255e6cdeddc
Steven Mackey <stevester94@gmail.com>
2021-11-19 17:02:01 -0800

    [logits] Using this going forward

--- logit_issue/minimal.py
+++ logit_issue/minimal.py
@@ -3,73 +3,9 @@ import torch
 import numpy as np
 import pickle
 
-from steves_utils.oshea_RML2016_ds import OShea_RML2016_DS
-
 import torch.nn as nn
 import torch.optim as optim
 
-
-# # Parameters relevant to results
-# RESULTS_DIR = "./results"
-# BEST_MODEL_PATH = os.path.join(RESULTS_DIR, "best_model.pth")
-# LOSS_CURVE_PATH = os.path.join(RESULTS_DIR, "loss.png")
-# EXPERIMENT_JSON_PATH = os.path.join(RESULTS_DIR, "experiment.json")
-
-# # Parameters relevant to experiment
-# NUM_LOGS_PER_EPOCH = 5
-
-# if not os.path.exists(RESULTS_DIR):
-#     os.mkdir(RESULTS_DIR)
-
-###################################
-# Parse Args, Set paramaters
-###################################
-# if len(sys.argv) > 1 and sys.argv[1] == "-":
-#     parameters = json.loads(sys.stdin.read())
-# elif len(sys.argv) == 1:
-#     fake_args = {}
-#     fake_args["experiment_name"] = "OShea SNR CNN"
-#     fake_args["lr"] = 0.001
-#     fake_args["n_epoch"] = 100
-#     fake_args["batch_size"] = 128
-#     fake_args["patience"] = 100
-#     fake_args["seed"] = 1337
-#     fake_args["device"] = "cuda"
-
-#     fake_args["source_snrs"] = [-18, -12, -6, 0, 6, 12, 18]
-#     fake_args["target_snrs"] = [2, 4, 8, 10, -20, 14, 16, -16, -14, -10, -8, -4, -2]
-
-#     fake_args["normalize_domain"] = True
-
-#     fake_args["x_net"] = [
-#         {"class": "Conv1d", "kargs": { "in_channels":2, "out_channels":50, "kernel_size":7, "stride":1, "padding":0 },},
-#         {"class": "ReLU", "kargs": {"inplace": True}},
-#         {"class": "Conv1d", "kargs": { "in_channels":50, "out_channels":50, "kernel_size":7, "stride":2, "padding":0 },},
-#         {"class": "ReLU", "kargs": {"inplace": True}},
-#         {"class": "Dropout", "kargs": {"p": 0.5}},
-#         {"class": "Flatten", "kargs": {}},
-#         {"class": "Linear", "kargs": {"in_features": 50*58, "out_features": 256}},
-#         {"class": "ReLU", "kargs": {"inplace": True}},
-#         {"class": "Dropout", "kargs": {"p": 0.5}},
-#         {"class": "Linear", "kargs": {"in_features": 256, "out_features": 80}},
-#         {"class": "ReLU", "kargs": {"inplace": True}},
-#         {"class": "Linear", "kargs": {"in_features": 80, "out_features": 11}},
-#     ]
-
-#     parameters = fake_args
-
-
-# experiment_name            = parameters["experiment_name"]
-# lr                         = parameters["lr"]
-# n_epoch                    = parameters["n_epoch"]
-# batch_size                 = parameters["batch_size"]
-# patience                   = parameters["patience"]
-# seed                       = parameters["seed"]
-# device                     = torch.device(parameters["device"])
-# source_snrs                = parameters["source_snrs"]
-# target_snrs                = parameters["target_snrs"]
-
-# start_time_secs = time.time()
 
 ###################################
 # Parameters
@@ -155,13 +91,14 @@ modulation_mapping = {
 data = []
 for mod in mods:
     for snr in snrs:
-        for x in Xd[(mod,snr)]:
-            data.append(
-                (
-                    x.astype(np.single),
-                    modulation_mapping[mod],
+        if snr in [-18, -12, -6, 0, 6, 12, 18]:
+            for x in Xd[(mod,snr)]:
+                data.append(
+                    (
+                        x.astype(np.single),
+                        modulation_mapping[mod],
+                    )
                 )
-            )
 
 
 
