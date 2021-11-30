4337f363e559d51b5b61aec316c0854641500827
Steven Mackey <stevester94@gmail.com>
2021-11-29 16:36:16 -0800

    Partial of trial 4

---
+++ oracle/one_distance_cnn/results/each_distance_each_run_stride_1/trial_4/6/replay.sh
@@ -0,0 +1,133 @@
+#! /bin/sh
+export PYTHONPATH=/usr/local/lib/python3/dist-packages:/usr/local/lib/python3.6/dist-packages
+cat << EOF | ./run.sh -
+{
+  "window_length": 256,
+  "num_examples_per_device": 75000,
+  "experiment_name": "One Distance ORACLE CNN",
+  "lr": 0.0001,
+  "n_epoch": 1000,
+  "batch_size": 256,
+  "patience": 10,
+  "device": "cuda",
+  "desired_serial_numbers": [
+    "3123D52",
+    "3123D65",
+    "3123D79",
+    "3123D80",
+    "3123D54",
+    "3123D70",
+    "3123D7B",
+    "3123D89",
+    "3123D58",
+    "3123D76",
+    "3123D7D",
+    "3123EFE",
+    "3123D64",
+    "3123D78",
+    "3123D7E",
+    "3124E4A"
+  ],
+  "seed": 1234,
+  "x_net": [
+    {
+      "class": "Conv1d",
+      "kargs": {
+        "in_channels": 2,
+        "out_channels": 50,
+        "kernel_size": 7,
+        "stride": 1,
+        "padding": 0,
+        "groups": 2
+      }
+    },
+    {
+      "class": "ReLU",
+      "kargs": {
+        "inplace": true
+      }
+    },
+    {
+      "class": "Conv1d",
+      "kargs": {
+        "in_channels": 50,
+        "out_channels": 50,
+        "kernel_size": 7,
+        "stride": 1,
+        "padding": 0
+      }
+    },
+    {
+      "class": "ReLU",
+      "kargs": {
+        "inplace": true
+      }
+    },
+    {
+      "class": "Dropout",
+      "kargs": {
+        "p": 0.5
+      }
+    },
+    {
+      "class": "Flatten",
+      "kargs": {}
+    },
+    {
+      "class": "Linear",
+      "kargs": {
+        "in_features": 5800,
+        "out_features": 256
+      }
+    },
+    {
+      "class": "ReLU",
+      "kargs": {
+        "inplace": true
+      }
+    },
+    {
+      "class": "Dropout",
+      "kargs": {
+        "p": 0.5
+      }
+    },
+    {
+      "class": "Linear",
+      "kargs": {
+        "in_features": 256,
+        "out_features": 80
+      }
+    },
+    {
+      "class": "ReLU",
+      "kargs": {
+        "inplace": true
+      }
+    },
+    {
+      "class": "Dropout",
+      "kargs": {
+        "p": 0.5
+      }
+    },
+    {
+      "class": "Linear",
+      "kargs": {
+        "in_features": 80,
+        "out_features": 17
+      }
+    }
+  ],
+  "desired_runs": [
+    2
+  ],
+  "window_stride": 25,
+  "source_domains": [
+    56
+  ],
+  "target_domains": [
+    56
+  ]
+}
+EOF
